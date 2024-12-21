2913069b51ca906a2aca2bbafc694c345572254c
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-21 17:49:44 +0530

    refactor(header): added welcome message to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderLogo from "@/components/header-logo";
 import Navigation from "@/components/navigation";
-import { ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import WelcomeMessage from "@/components/welcome-message";
 
 function Header() {
   return (
     <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
-      <div className="max-w-screen-2xl">
+      <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16 ">
             <HeaderLogo />
@@ -15,7 +17,11 @@ function Header() {
           <ClerkLoaded>
             <UserButton />
           </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-slate-400" />
+          </ClerkLoading>
         </div>
+        <WelcomeMessage />
       </div>
     </header>
   );
