f36561839b8facd183f66ee0ebd7288e7a4393ab
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-23 14:36:10 +0530

    feat(transactions): added import csv functionality

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -6,19 +6,71 @@ import { DataTable } from "@/components/data-table";
 import { Loader2, Plus } from "lucide-react";
 
 import { columns } from "./columns";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
 
+import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
+
+import { transactions as transactionSchema } from "@/db/schema";
+import { useState } from "react";
+import { toast } from "sonner";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 function TransactionsPage() {
+  const [AccountDialog, confirm] = useSelectAccount();
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
   const newTransaction = useNewTransaction();
+  const createTransactions = useBulkCreateTransactions();
   const deleteTransactions = useBulkDeleteTransactions();
   const transactionsQuery = useGetTransactions();
   const transactions = transactionsQuery.data || [];
 
   const isDisabled =
     transactionsQuery.isLoading || deleteTransactions.isPending;
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    console.log(results);
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
+  const onSubmitImport = async (
+    values: (typeof transactionSchema.$inferInsert)[]
+  ) => {
+    const accountId = await confirm();
+    if (!accountId) return toast.error("Please select an account to continue");
+
+    const data = values.map((value) => ({
+      ...value,
+      accountId: accountId as string,
+    }));
+    createTransactions.mutate(data, {
+      onSuccess: () => {
+        onCancelImport();
+      },
+    });
+  };
+
   if (transactionsQuery.isLoading) {
     return (
       <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -35,6 +87,19 @@ function TransactionsPage() {
       </div>
     );
   }
+
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <AccountDialog />
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={onSubmitImport}
+        />
+      </>
+    );
+  }
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -42,10 +107,17 @@ function TransactionsPage() {
           <CardTitle className="text-xl line-clamp-1">
             Transaction History
           </CardTitle>
-          <Button onClick={newTransaction.onOpen}>
-            <Plus className="size-4 mr-2" />
-            <span>Add new</span>
-          </Button>
+          <div className="flex items-center gap-x-2 flex-col lg:flex-row gap-y-2">
+            <Button
+              onClick={newTransaction.onOpen}
+              size="sm"
+              className="w-full lg:w-auto"
+            >
+              <Plus className="size-4 mr-2" />
+              <span>Add new</span>
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
---
+++ features/accounts/hooks/use-select-account.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { CustomSelect } from "@/components/custom-select";
+
+export const useSelectAccount = (): [
+  () => JSX.Element,
+  () => Promise<unknown>
+] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: string | undefined) => void;
+  } | null>(null);
+
+  const selectValue = useRef<string>();
+
+  const accountQuery = useGetAccounts();
+  const accountMutation = useCreateAccount();
+  const onCreateAccount = (name: string) => accountMutation.mutate({ name });
+  const accountOptions = (accountQuery.data ?? []).map((account) => ({
+    label: account.name,
+    value: account.id,
+  }));
+
+  const confirm = () =>
+    new Promise((resolve) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(selectValue.current);
+    handleClose();
+  };
+
+  const handleCancel = () => {
+    promise?.resolve(undefined);
+    handleClose();
+  };
+
+  const AccountDialog = () => {
+    return (
+      <Dialog open={promise !== null}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Select Account</DialogTitle>
+            <DialogDescription>
+              Please select an account to continue
+            </DialogDescription>
+          </DialogHeader>
+          <CustomSelect
+            placeholder="Select an account"
+            options={accountOptions}
+            onCreate={onCreateAccount}
+            onChange={(value) => (selectValue.current = value)}
+            disabled={accountQuery.isLoading || accountMutation.isPending}
+          />
+          <DialogFooter className="pt-2">
+            <Button onClick={handleCancel} variant="outline">
+              Cancel
+            </Button>
+            <Button onClick={handleConfirm}>Confirm</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  return [AccountDialog, confirm];
+};
